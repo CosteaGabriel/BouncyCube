c16887e1ff83a8dca437cf1e9b26f8a038360940
CosteaGabriel <costea.gabriel19@gmail.com>
2017-03-05 19:43:15 +0200

    New Project

---
+++ app/src/main/java/com/example/gabi/bouncycube/BouncyCubeActivity.java
@@ -0,0 +1,12 @@
+package com.example.gabi.bouncycube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BouncyCubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/gabi/bouncycube/Cube.java
@@ -0,0 +1,7 @@
+package com.example.gabi.bouncycube;
+
+/**
+ * Created by Gabi on 3/5/2017.
+ */
+public class Cube {
+}
